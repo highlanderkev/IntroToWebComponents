f579faec7bb215d9d06a0e56c55dbf28df62b72a
Kevin Patrick Westropp <mail@kevinpatrickwestropp.com>
2017-03-31 17:17:49 +0000

    More changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,32 @@
 "use strict";
 
 var gulp = require("gulp"),
-    connect = require("gulp-connect");
-    
-    
-gulp.task("serve", function(){
+    path = require("path"),
+    connect = require("gulp-connect"),
+    vulcanize = require("gulp-vulcanize"),
+    crisper = require("gulp-crisper");
+
+gulp.task("serve", function () {
     connect.server({
-        root:"./",
+        root: "./",
         livereload: false
     });
+});
+
+
+gulp.task("bundle", function () {
+    return gulp.src("src/imports/index.html")
+        .pipe(vulcanize({
+            abspath: "",
+            excludes: [],
+            stripExcludes: false,
+            inlineScripts: false
+        }))
+        .pipe(crisper({
+            scriptInHead: false,
+            onlySplit: false
+        }))
+        .pipe(gulp.dest("src/bundle"));
 });
 
 gulp.task("default", ["serve"]);
